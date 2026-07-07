aa4d150cb201f538211736b7a8778d4ad5f677b4
Aleff Correia <aleffcorreia@outlook.com>
2026-07-06 23:48:31 -0300

    add enums

---
+++ TaskManager/Enums/MenuOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Enums
+{
+    public enum MenuOption
+    {
+        User = 1,
+        Task = 2,
+        Dashboard = 3,
+        Exit = 0
+    }
+}
---
+++ TaskManager/Enums/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Enums
+{
+   public enum TaskStatus
+    {
+        Pedding = 1,
+        InProgress = 2,
+        Complete = 3,
+        Canceled = 4
+
+    }
+}
